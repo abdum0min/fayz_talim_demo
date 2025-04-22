ceb47f4d05ae79ade88d4b8f6464f08d6f7c32c2
abdum0min <abduganiabdurasulov5150@gmail.com>
2025-04-23 00:56:06 +0500

    modal added

--- assets/main/js/main.js
+++ assets/main/js/main.js
@@ -25,9 +25,51 @@ window.addEventListener('DOMContentLoaded', ()=>{
       window.addEventListener('resize', updateCarousel);
     
       updateCarousel();
-    }
+  }
     
-    initializeCarousel('.carousel_track_t', '#PrevBtn', '#NextBtn');
+  initializeCarousel('.carousel_track_t', '#PrevBtn', '#NextBtn');
+
+  function openModal(modalContentSelector, modalSelector,){
+    const modalContent = document.querySelector(modalContentSelector),
+        modal = document.querySelector(modalSelector)
+    modal.classList.remove('hide')
+    modal.classList.add('show')
+    document.body.style.overflow = "hidden"
+    modalContent.classList.add('modal_fade')
+  }
+
+  function closeModal(modalSelector){
+    const modal = document.querySelector(modalSelector)
+    modal.classList.remove('show')
+    modal.classList.add('hide')
+    document.body.style.overflowY = 'scroll'
+  }
+
+  function modal(btnSelector, modalSelector, modalContentSelector){
+    // Modal
+    const modalOpenBtn = document.querySelectorAll(btnSelector),
+        modal = document.querySelector(modalSelector)
+        
+
+    
+    modalOpenBtn.forEach(item=>{
+        item.addEventListener('click',()=> openModal(modalContentSelector,modalSelector))
+    })
+
+
+    modal.addEventListener('click', (event)=>{
+        if(event.target === modal || event.target.getAttribute('data-modal-close') === ''){
+            closeModal(modalSelector)
+        }
+    })
+
+    document.addEventListener("keydown", (event)=>{
+        if(event.code === 'Escape' && modal.classList.contains('show') ){
+            closeModal(modalSelector)
+        }
+    })
+  }
+  modal('[data-modal]', '.modal', '.modal__content' )
 })
 
   
